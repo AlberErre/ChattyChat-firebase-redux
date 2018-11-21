faa9a78690960c5d4aea957054a64269e4cbe5a3
Alber Erre <arblugo@gmail.com>
2018-11-21 20:02:11 +0000

    add SendButton component

---
+++ src/components/SendButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const SendButton = (props) => {
+  return (
+    <div>
+      <button data-user="Alber">Send</button>
+    </div>
+  ); 
+};
+
+export default SendButton;
