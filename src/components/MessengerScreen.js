41c334669185c08710cca2354dbbd0b8a26d3e88
Alber Erre <arblugo@gmail.com>
2018-11-22 12:35:00 +0000

    init MessengerScreen

---
+++ src/components/MessengerScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../App.css";
+
+const MessengerScreen = () => {
+    
+  return <div id="div-chat" className="chattyBox"></div>
+};
+
+export default MessengerScreen;
