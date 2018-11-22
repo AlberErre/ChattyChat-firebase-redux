6921e26967b0d3ca9838f318d4a049c3d4431ba1
Alber Erre <arblugo@gmail.com>
2018-11-22 14:04:03 +0000

    init logo component

---
+++ src/components/ChattyLogo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../App.css";
+
+const ChattyLogo = () => {
+    
+  return <img className="chattyIcon" src={require('../assets/chattyChat.png')} alt="ChattyChat Logo" /> 
+};
+
+export default ChattyLogo;
