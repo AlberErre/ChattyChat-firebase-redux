4c12c5bdbd74323a60baf7f2688751f14b78969a
Alber Erre <arblugo@gmail.com>
2018-11-22 12:18:37 +0000

    init TextInput

---
+++ src/components/TextInput.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+
+const TextInput = () => {
+
+  return  <input type="text" name="" placeholder="Chatty here..." id="in-message" /> 
+};
+
+export default TextInput;
