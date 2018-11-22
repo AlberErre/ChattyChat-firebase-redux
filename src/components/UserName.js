479b228323d900c956054d7ccf92d23ef57c7585
Alber Erre <arblugo@gmail.com>
2018-11-22 14:36:01 +0000

    init UserName input

---
+++ src/components/UserName.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../App.css";
+
+const UserName = () => {
+    
+  return  <input type="text" name="" className="userNameField" placeholder="Your name..." id="in-user-name" /> 
+};
+
+export default UserName;
