162d698c3286990d07aef97f4066f10f957b0cec
Alber Erre <arblugo@gmail.com>
2018-11-23 02:48:55 +0000

    update format (ChattyLogo)

--- src/components/ChattyLogo.js
+++ src/components/ChattyLogo.js
@@ -2,8 +2,13 @@ import React from "react";
 import "../App.css";
 
 const ChattyLogo = () => {
-    
-  return <img className="chattyIcon" src={require('../assets/chattyChat.png')} alt="ChattyChat Logo" /> 
+  return (
+    <img
+      className="chattyIcon"
+      src={require("../assets/chattyChat.png")}
+      alt="ChattyChat Logo"
+    />
+  );
 };
 
 export default ChattyLogo;
